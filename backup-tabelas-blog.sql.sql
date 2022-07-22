75f326525d11553215f58257d7510cf6363b87d5
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-07-22 09:39:28 -0300

    modelagem física do banco

---
+++ backup-tabelas-blog.sql.sql
@@ -0,0 +1,128 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 22-Jul-2022 às 14:20
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `microblog_thiagoribeiro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` smallint(6) NOT NULL,
+  `nome` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `noticias`
+--
+
+CREATE TABLE `noticias` (
+  `id` mediumint(9) NOT NULL,
+  `data` datetime NOT NULL DEFAULT current_timestamp(),
+  `titulo` varchar(150) NOT NULL,
+  `texto` text NOT NULL,
+  `resumo` tinytext NOT NULL,
+  `imagem` varchar(45) NOT NULL,
+  `destaque` enum('sim','nao') NOT NULL,
+  `usuario_id` smallint(6) DEFAULT NULL,
+  `categoria_id` smallint(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` smallint(6) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  `tipo` enum('admin','editor') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `noticias`
+--
+ALTER TABLE `noticias`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_noticias_usuarios` (`usuario_id`),
+  ADD KEY `fk_noticias_categorias` (`categoria_id`);
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `noticias`
+--
+ALTER TABLE `noticias`
+  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `noticias`
+--
+ALTER TABLE `noticias`
+  ADD CONSTRAINT `fk_noticias_categorias` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_noticias_usuarios` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- bd.md
+++ bd.md
@@ -2,16 +2,43 @@
 CREATE TABLE usuarios (
     id SMALLINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(45) NOT NULL,
-    email VARCHAR(45),
-    senha VARCHAR(45),
-    tipo ENUM ('admin', 'editor')
+    email VARCHAR(45) NOT NULL UNIQUE,
+    senha VARCHAR(45) NOT NULL,
+    tipo ENUM ('admin', 'editor') NOT NULL
+);
+```
+
+```sql
+CREATE TABLE categorias(
+    id SMALLINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL
 );
 ```
 
 ```sql
 CREATE TABLE noticias (
     id MEDIUMINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    data DATETIME NOT NULL,
-    titulo VARCHAR(100) NOT NULL,
-)
+    data DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    titulo VARCHAR(150) NOT NULL, 
+    texto TEXT NOT NULL,
+    resumo TINYTEXT NOT NULL ,
+    imagem VARCHAR(45) NOT NULL,
+    destaque ENUM('sim','nao') NOT NULL,
+    usuario_id SMALLINT NULL,
+    categoria_id SMALLINT NULL
+);
+```
+
+```sql
+ALTER TABLE noticias
+    ADD CONSTRAINT fk_noticias_usuarios
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+    ON DELETE SET NULL ON UPDATE NO ACTION;
+```
+
+```sql
+ALTER TABLE noticias
+    ADD CONSTRAINT fk_noticias_categorias
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+    ON DELETE SET NULL ON UPDATE NO ACTION;
 ```
