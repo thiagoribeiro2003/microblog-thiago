0c816c9f67339128e459568336755989ea5aad28
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-07-25 08:29:02 -0300

    Mudando o nome do banco de dados.
    O nome da conexão do Banco de dados na classe Banco.php, tem que ser igual o nome do banco de dados no sql

--- src/Banco.php
+++ src/Banco.php
@@ -14,7 +14,7 @@ de acesso ao servidor de Banco de Dados */
 private static string $servidor = "localhost";
 private static string $usuario = "root";
 private static string $senha = "";
-private static string $banco = "microblog_thiagoribeiro";
+private static string $banco = "microblog-thiago";
 private static PDO $conexao; // precisa do use PDO 
 /*private static \PDO $conexao; // se for fazer com \ não precisa deo use PDO*/
 
