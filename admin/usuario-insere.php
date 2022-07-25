b97899337665e900ec5bc3e6f4610b33d408cdb9
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-07-25 09:13:25 -0300

    Criando função de inserir, criando getters e setters, e criando função de senha

--- admin/usuario-insere.php
+++ admin/usuario-insere.php
@@ -1,5 +1,22 @@
 <?php 
-require_once "../inc/cabecalho-admin.php";
+use Microblog\Usuario;
+require_once "../inc/cabecalho-admin.php"; // REQUIRE_ONCE = CHAMA UMA ÚNICA VEZ 
+
+
+if(isset($_POST['inserir'])){
+	$usuario = new Usuario;
+
+	$usuario->setNome($_POST['nome']);
+	$usuario->setEmail($_POST['email']);
+	$usuario->setTipo($_POST['tipo']);
+
+	$usuario->setSenha($_POST['senha']);
+	echo $usuario->getSenha();
+
+	// $usuario->inserir();
+	// header("location:usuarios.php");
+
+}
 ?>
 
 
--- src/Usuario.php
+++ src/Usuario.php
@@ -29,9 +29,29 @@ final class Usuario {
         return $resultado;
     }
 
-}
+    public function inserir():void {
+        $sql = "INSERT INTO usuarios(nome, email, senha, tipo)
+        VALUES(:nome, :email, :senha, :tipo)";
 
-/* 
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindParam(":nome", $this->nome, PDO::PARAM_STR);
+            $consulta->bindParam(":email", $this->email, PDO::PARAM_STR);
+            $consulta->bindParam(":senha", $this->senha, PDO::PARAM_STR);
+            $consulta->bindParam(":tipo", $this->tipo, PDO::PARAM_STR);
+        } catch (\Throwable $erro) {
+            die("Erro: ".$erro->getMessage());
+        }
+        
+    }
+
+
+    public function codificaSenha(string $senha):string {
+        return password_hash($senha, PASSWORD_DEFAULT);
+    }
+
+
+    /* 
 try {
 
 } catch (Exception $erro) {
@@ -44,3 +64,69 @@ try {
 // } catch (Exception $erro) {
 //     die("Erro: ". $erro->getMessage());
 // }
+
+
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    
+    public function setId(int $id)
+    {
+        $this->id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+    }
+
+//===========================================================
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome)
+    {
+        $this->nome = filter_var($nome, FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+
+//===========================================================
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email)
+    {
+        $this->email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    }
+
+    
+//===========================================================
+   
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+
+    public function setSenha(string $senha)
+    {
+        $this->senha = filter_var($senha, FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+
+//===========================================================
+
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+
+    public function setTipo(string $tipo)
+    {
+        $this->tipo = filter_var($tipo, FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+
+//===========================================================
+}
+
