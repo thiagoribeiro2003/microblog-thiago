b092e004634388471f8095125585d2cedce5ea49
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-07-27 08:47:29 -0300

    Programação de exclusão de usuário

--- admin/usuario-exclui.php
+++ admin/usuario-exclui.php
@@ -1,7 +1,17 @@
 <?php
-require_once "../inc/cabecalho-admin.php";
+require_once "../vendor/autoload.php";
 use Microblog\Usuario;
-$usuario = new Usuario;
+
+// Criamos um objetos para poder acessar os recursos da Classe
+$usuario = new Usuario; // Não esqueça do autoload e do namespace
+
+// Obtemos o id da URL e o passamos para o setter
 $usuario->setId($_GET['id']);
+
+// Só então executamos o método de exclusão
 $usuario->excluirUsuario();
+
+// Após excluir, redirecionamos para a página de lista de usuários
 header("location:usuarios.php");
+
+// Para usar recursos de uma classe precisa criar um objeto
